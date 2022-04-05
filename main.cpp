47f998a2656df0f0e92c4dc4e3074e94b29a1948
Mason Wooley <mason.wooley@gmail.com>
2022-04-05 19:32:26 -0400

    Add comments and clean up code style

--- main.cpp
+++ main.cpp
@@ -4,30 +4,43 @@
 #define SCREEN_WIDTH 1280 
 #define SCREEN_HEIGHT 720
 
-int main(int argc, char** argv){
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+int main (int argc, char** argv) {
+    // Exit the program if SDL fails to initialize
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
         return 1;
     }
 
+    // Create a window and store the reference to it
     SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    if(!window){
+
+    // Exit if the window fails to create
+    if (!window) {
         printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
         return 1;
     }
 
+    // Create a renderer and store the reference to it
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if(!renderer){
+
+    // Exit if the renderer fails to create
+    if (!renderer) {
         printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
         return 1;
     }
 
     bool running = true;
-    while(running){
+
+    // Game loop 
+    while (running) {
+
         SDL_Event event;
-        while(SDL_PollEvent(&event)){
-            switch(event.type){
+
+        // Poll the event queue if events are queued
+        while (SDL_PollEvent(&event)) {
+            switch(event.type) {
                 case SDL_QUIT:
+                    // Break the game loop on next iteration
                     running = false;
                     break;
 
@@ -36,9 +49,13 @@ int main(int argc, char** argv){
             }
         }
 
+        // Set the render color to cyan
         SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+        
+        // Clear the screen of existing data and draw the render color
         SDL_RenderClear(renderer);
 
+        // Update the screen with the drawn render data
         SDL_RenderPresent(renderer);
     }
 
