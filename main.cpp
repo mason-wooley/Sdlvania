deb21d2de98adeb30e22f90f92ad3a3783db0999
Mason Wooley <mason.wooley@gmail.com>
2022-04-08 15:44:08 -0400

    Separe main into multiple functions

--- main.cpp
+++ main.cpp
@@ -4,30 +4,34 @@
 #define SCREEN_WIDTH 1280 
 #define SCREEN_HEIGHT 720
 
+// Start SDL and create the window
+bool init();
+
+// Load media objects into the window
+bool loadMedia();
+
+// Release media and shut down SDL
+void close();
+
+// Globally declare window
+SDL_Window* gWindow = NULL;
+
+// The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+// The renderer to draw to
+// SDL_Renderer* gRenderer = NULL;
+
+// The image to load to the scren
+SDL_Surface* gCharacter = NULL;
+
 int main (int argc, char** argv) {
-    // Exit the program if SDL fails to initialize
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
 
-    // Create a window and store the reference to it
-    SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    init();
 
-    // Exit if the window fails to create
-    if (!window) {
-        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
 
-    // Create a renderer and store the reference to it
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    // Exit if the renderer fails to create
-    if (!renderer) {
-        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+
 
     bool running = true;
 
@@ -60,4 +64,38 @@ int main (int argc, char** argv) {
     }
 
     return 0;
+}
+
+bool init() {
+    // Initialization status
+    bool success = true;
+
+    // Initialize SDL
+    if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
+        success = false;
+    } else {
+        // Create a window and store the reference to it
+        gWindow = SDL_CreateWindow("Sdlvania", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+        
+        // Fail if the window isn't created
+        if ( gWindow == NULL ) {
+            printf("Error: Failed to open window.\nSDL Error: '%s'\n", SDL_GetError());
+            success = false;
+        } else {
+            // Get the screen surface
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
+
+        // Create a renderer and store the reference to it
+        // gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+
+        // Fail if the renderer isn't created
+        /*
+        if ( !gRenderer ) {
+            printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
+            return 1;
+        }
+        */
+    }
 }
