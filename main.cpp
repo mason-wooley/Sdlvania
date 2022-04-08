cc0177838fa55e296fbb18d0ef8a866ecefccf24
Mason Wooley <mason.wooley@gmail.com>
2022-04-08 19:52:45 -0400

    Add character surface and input

--- main.cpp
+++ main.cpp
@@ -25,27 +25,67 @@ SDL_Surface* gScreenSurface = NULL;
 // The image to load to the scren
 SDL_Surface* gCharacter = NULL;
 
-int main (int argc, char** argv) {
-
-    init();
-
-
-
-
+int main ( int argc,  char** args ) {
+    // Start SDL
+    if ( init() ) {
+        // Load media
+        if ( !loadMedia() ) {
+            return 1;
+        }
+    } else {
+        return 1;
+    }
 
     bool running = true;
 
+    // The rectangle defining the character position
+    SDL_Rect* rect;
+
+    rect->x = SCREEN_WIDTH / 2 - gCharacter->w / 2;
+    rect->y = SCREEN_HEIGHT / 2 - gCharacter->h / 2;
+    
+    bool leftKeyDown = false;
+    bool rightKeyDown = false;
+    bool upKeyDown = false;
+    bool downKeyDown = false;
+    
     // Game loop 
     while (running) {
+        const Uint8* state = SDL_GetKeyboardState(NULL);
 
         SDL_Event event;
 
         // Poll the event queue if events are queued
         while (SDL_PollEvent(&event)) {
-            switch(event.type) {
+            switch (event.type) {
                 case SDL_QUIT:
                     // Break the game loop on next iteration
                     running = false;
+                    close();
+                    break;
+
+                case SDL_KEYDOWN:
+                    if (state[SDL_SCANCODE_LEFT]) {
+                        leftKeyDown = true;
+                    } else if (state[SDL_SCANCODE_RIGHT]) {
+                        rightKeyDown = true;
+                    } else if (state[SDL_SCANCODE_UP]) {
+                        upKeyDown = true;
+                    } else if (state[SDL_SCANCODE_DOWN]) {
+                        downKeyDown = true;
+                    }
+                    break;
+                
+                case SDL_KEYUP:
+                    if (state[SDL_SCANCODE_LEFT]) {
+                        leftKeyDown = false;
+                    } else if (state[SDL_SCANCODE_RIGHT]) {
+                        rightKeyDown = false;
+                    } else if (state[SDL_SCANCODE_UP]) {
+                        upKeyDown = false;
+                    } else if (state[SDL_SCANCODE_DOWN]) {
+                        downKeyDown = false;
+                    }
                     break;
 
                 default:
@@ -53,14 +93,44 @@ int main (int argc, char** argv) {
             }
         }
 
+        if (leftKeyDown == true) {
+            rect->x = 20;
+
+        }
+        
+        if (rightKeyDown == true) {
+            rect->x = 400;
+        }
+        
+        /*
+        if (upKeyDown == true) {
+            rect->y -= 1;
+        }
+        
+        if (downKeyDown == true) {
+            rect->y += 1;
+        }
+        */
+
+        // Clear the screen to black
+        SDL_FillRect( gScreenSurface, NULL, SDL_MapRGB(gScreenSurface->format, 0x00, 0x00, 0x00));
+
+        // Apply the image
+        SDL_BlitSurface(gCharacter, NULL, gScreenSurface,  rect);
+
+        // Update the surface
+        SDL_UpdateWindowSurface( gWindow );
+        
+        /*
         // Set the render color to cyan
-        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+        SDL_SetRenderDrawColor(gRenderer, 0, 255, 255, 255);
         
         // Clear the screen of existing data and draw the render color
-        SDL_RenderClear(renderer);
+        SDL_RenderClear(gRenderer);
 
         // Update the screen with the drawn render data
-        SDL_RenderPresent(renderer);
+        SDL_RenderPresent(gRenderer);
+        */
     }
 
     return 0;
@@ -71,16 +141,16 @@ bool init() {
     bool success = true;
 
     // Initialize SDL
-    if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
-        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
+    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+        printf( "Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError() );
         success = false;
     } else {
         // Create a window and store the reference to it
-        gWindow = SDL_CreateWindow("Sdlvania", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+        gWindow = SDL_CreateWindow( "Sdlvania", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0 );
         
         // Fail if the window isn't created
         if ( gWindow == NULL ) {
-            printf("Error: Failed to open window.\nSDL Error: '%s'\n", SDL_GetError());
+            printf( "Error: Failed to open window.\nSDL Error: '%s'\n", SDL_GetError() );
             success = false;
         } else {
             // Get the screen surface
@@ -98,4 +168,32 @@ bool init() {
         }
         */
     }
+}
+
+bool loadMedia() {
+    // Load media status
+    bool success = true;
+
+    // Load character image
+    gCharacter = SDL_LoadBMP( "media/character.bmp" );
+
+    if ( gCharacter == NULL ) {
+        printf( "Error: Failed to load image '%s'.\nSDL Error: '%s'\n", "media/character.bmp", SDL_GetError() );
+        success = false;
+    }
+
+    return success;
+}
+
+void close() {
+    // Deallocate surface
+    SDL_FreeSurface( gCharacter );
+    gCharacter = NULL;
+
+    // Destroy window
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+
+    // Quit SDL
+    SDL_Quit();
 }
